fc3834070259abefae018b6730d5bf42046aacdd
Ian Cornelius <icornelius@luc.edu>
2023-11-18 21:58:19 -0600

    rewrite converter.py without pandas
    - remove pandas dependency
    - add some comments on modules

--- converter.py
+++ converter.py
@@ -1,13 +1,26 @@
 import os
-import pandas as pd
 
 source_csv = 'line-number-concordance.csv'
 
 print("Welcome to the reference converter for Walton's Boethius.\n")
 
+# confirm file exists
+
 if not os.path.exists(source_csv):
   print("Source file not found.")
   exit()
+
+# load file as dictionary of lists
+
+with open(source_csv) as f:
+    data = f.readlines()
+dict_ = {}
+n=0
+for line in data:
+  dict_[str(n)] = line.strip().split(",")
+  n += 1
+
+# identify a source to query
 
 print("What reference system do you want to convert from?\n\n"
         "(1) Mark Science's continuous stanza numbering\n"
@@ -30,14 +43,14 @@ while valid == False:
     print("You have selected " + read + ", " + option_dict[int(read)] + "\n")
     valid = True
 
-dframe = pd.read_csv(source_csv)
+# load a query-term from input
 
 repeat = True
 
 while repeat == True:
     if read == "1" or read == "4":
         input_msg = "Enter a stanza number: "
-        ref = input(input_msg)
+        query = input(input_msg)
     elif read == "2":
         print(" * For the translation proper, use the format, e.g., \"3m9\", for the ninth meter of the third book\n"
               " * For the translator's FIRST PREFACE (preceding the Prologue), enter \"Pref1\"\n"
@@ -47,7 +60,7 @@ while repeat == True:
         section = input(input_msg)
         input_msg = "Enter a stanza number: "
         stanza = input(input_msg)
-        ref = section + "." + stanza
+        query = section + "." + stanza
     elif read == "3":
         input_msg = "Enter a book number (for initial prefatory material, enter \"P1\"; for the second preface enter \"P2\"): "
         section = input(input_msg)
@@ -59,14 +72,36 @@ while repeat == True:
         elif section == "P2" or section == "4" or section == "5":
             stanza = (int(line)-1) // 7 + 1
             floor_line = int(stanza) * 7 - 6
-        ref = str(section) + "." + str(floor_line)
+        query = str(section) + "." + str(floor_line)
     else:
         print("Sorry, that option is not yet available.")
         exit()
 
-    conversion_table = dframe[dframe[dframe.columns[int(read)]] == ref].drop(columns=['logical_ln.'])
-    print("Looking for reference point", ref, "in", option_dict[int(read)], "...")
-    print("\n", conversion_table, "\n")
+    print("Looking for reference point", query, "in", option_dict[int(read)], "...\n")
+
+    # Run the query
+    key=0
+    found=False
+
+    for key in range(len(dict_)):
+      found = dict_[str(key)][int(read)] == query
+      if found == True:
+          # print("Query item found at key", key, "\n")
+          break
+      key += 1
+
+    if found == False:
+        print("Query term not found")
+
+    # Print the conversion
+
+    else:
+        for i in range(4):
+            # print("\t" + dict_['0'][i+1] + ":\t" + dict_[str(key)][i+1])
+            print("\t" + option_dict[i+1] + ":\t" + dict_[str(key)][i+1])
+        print()
+
+    # Offer to query again
 
     input_msg = "Convert another reference from the same source? (y/n) "
     again = input(input_msg)
